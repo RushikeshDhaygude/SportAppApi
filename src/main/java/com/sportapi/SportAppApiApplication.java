d7964b1c953a100ccadbec58c1c2fb94deac6028
Rushikesh <rushikeshdhaygude9414@gmail.com>
2024-01-15 11:26:39 +0530

    Project Initial setup added.
    File structure created and database configuration added.

---
+++ src/main/java/com/sportapi/SportAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.sportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportAppApiApplication.class, args);
+	}
+
+}
